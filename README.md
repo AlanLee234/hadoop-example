68cfcbcbe80b75439dde00c5062f366e237ab969
HeWang <w1299495070@outlook.com>
2023-03-01 18:32:24 +0800

    1
    1

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ sbin/stop-all.sh
 
 
 
+hdfs dfs -copyFromLocal C:\Users\Administrator\Desktop\file01 /user/joe/wordcount/input/
+
+
+
 http://localhost:9870/
 
 hdfs: http://localhost:50070/dfshealth.html#tab-overview
---
+++ hs.cmd
@@ -0,0 +1,10 @@
+C:\env\apache-zookeeper-3.5.10-bin\bin\zkServer.cmd
+C:\env\apache-zookeeper-3.5.10-bin\bin\zkCli.cmd
+
+C:\env\hadoop-2.10.1\sbin\start-all.cmd
+@rem hdfs dfs -copyFromLocal C:\Users\Administrator\Desktop\file01 /user/joe/wordcount/input/
+
+
+@rem C:\env\hive-2.3.9\bin\hive.cmd
+
+
